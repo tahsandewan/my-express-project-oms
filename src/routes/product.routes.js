eec60c4b9ccfdd22eb226bf643464c4e31c0fd34
Tahsan Dewan <39862456+tahsandewan@users.noreply.github.com>
2025-04-21 18:20:29 +0600

    Update product.routes.js

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,14 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/product.controller");
-
-// router.get("/", userController.getAllUsers);
-// router.get("/search", userController.searchUser);
  router.get("/all-products", productController.allProducts);
-
-//  router.get("/:id", productController.getProduct);
 router.post("/", productController.createProduct);
-// router.put("/:id", userController.updateUser);
-// router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
